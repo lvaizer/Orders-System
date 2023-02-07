adbd4c0d74a4fbb1b8ca650fc5bd6cbca1294bb8
Liron Vaizer <lvaizer@gmail.com>
2023-02-07 09:28:35 +0200

    change the server URL

--- src/QueryFactory.js
+++ src/QueryFactory.js
@@ -1,6 +1,6 @@
 import {useQuery} from 'react-query';
 
-const BASE_URL = window.location.hostname === "localhost" ? 'http://localhost:3002' : 'https://orders-server-3wp3.onrender.com'
+const BASE_URL = window.location.hostname === "localhost" ? 'http://localhost:3002' : 'https://orders-server-6lz7.onrender.com'
 
 export const useGetDates = () => useQuery('dates', () => fetch(BASE_URL + '/orders/dates').then(res => res.json()));
 
