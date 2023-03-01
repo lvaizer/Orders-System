050ff8f9cdd8101f975dc8d39c3df30e3a615a93
Liron Vaizer <lvaizer@gmail.com>
2023-03-01 12:44:57 +0200

    actions

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ export default function App() {
     return (
         <>
             <Routes>
-                <Route path="/" element={<MainLayout/>}>
+                <Route path="/orders_system" element={<MainLayout/>}>
                     <Route index element={<Home/>}/>
                     {ordersRouter}
                     <Route path="*" element={<NotFound/>}/>
--- src/modules/orders/OrdersRouter.js
+++ src/modules/orders/OrdersRouter.js
@@ -1,7 +1,7 @@
 import {Route} from "react-router-dom";
 import OrdersMainContainer from "./OrdersMainContainer";
 
-export default [<Route key="orders_router" path="/orders" element={<OrdersMainContainer/>}>
+export default [<Route key="orders_router" path="/orders_system/orders" element={<OrdersMainContainer/>}>
     <Route path=":year" element={<></>}/>
     <Route path=":year/:month" element={<></>}/>
     <Route path=":year/:month/:day" element={<></>}/>
