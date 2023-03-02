d52d3e6b0bd81c04bfab5d7575f864d3f8d163fc
Liron Vaizer <lvaizer@gmail.com>
2023-03-02 12:36:48 +0200

    fix router for

--- src/QueryFactory.js
+++ src/QueryFactory.js
@@ -10,8 +10,9 @@ const BASE_URL = window.location.hostname === "localhost" ? 'http://localhost:30
 * */
 export const useGetDates = () => useQuery('dates', () => axios(BASE_URL + '/orders/dates').then(res => res.data));
 
-export const useGetOrders = (year, month, day) => useQuery(['orders', year, month, day], () => axios(BASE_URL + '/orders/' + year + '/' + month + '/' + day).then(res => res.data));
-
+export const useGetOrders = (year, month, day) => {
+    return useQuery(['orders', year, month, day], () => axios(BASE_URL + '/orders/' + year + '/' + month + '/' + day).then(res => res.data));
+}
 export const PostNewOrder = () => useMutation((data) => axios.post(BASE_URL + '/orders/new/order', data).then(res => res.data));
 export const UpdateOrder = () => useMutation((data) => axios.post(BASE_URL + '/orders/update', data).then(res => res.data));
 export const DeleteOrder = () => useMutation((data) => axios.post(BASE_URL + '/orders/delete', data).then(res => res.data));
