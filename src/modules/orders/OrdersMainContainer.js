7bc7128febc0a85376345db5086d22b7b923288d
Liron Vaizer <lvaizer@gmail.com>
2023-03-12 18:09:31 +0200

    test

--- src/modules/orders/OrdersMainContainer.js
+++ src/modules/orders/OrdersMainContainer.js
@@ -26,83 +26,84 @@ export default function OrdersMainContainer() {
     const [days, setDays] = useState();
 
 
-    const {
-        data: dates,
-        error: errorDates,
-        isLoading: isLoadingDates,
-        refetch: refetchDates
-    } = useGetDates();
-
-    const {
-        data: orders,
-        isLoading: isLoadingOrders,
-        error: errorOrders,
-        refetch: refetchOrders
-    } = useGetOrders(year, month, day);
-
-    const refresh = useCallback(() => {
-        refetchDates().then();
-        refetchOrders().then();
-    }, [refetchDates, refetchOrders]);
-
-    const isDay = () => day;
-    const isMonth = () => month && !day;
-    const isYear = () => year && !month;
-
-    useEffect(() => {
-        if (dates && dates.status && dates.message) {
-            setYears(getYearsListFromServerResponse(dates, year));
-            setMonths(getMonthsListFromServerResponse(dates, year, month))
-            setDays(getDaysListFromServerResponse(dates, year, month, day));
-        }
-    }, [dates, year, month, day]);
-
-    function getSideListElements() {
-        if (!dates) return;
-        if (isYear()) return years;
-        if (isMonth()) return months;
-        if (isDay()) return days;
-        return years;
-    }
-
-    function getMainListElements() {
-        if (isYear()) return months;
-        if (isMonth()) return days;
-        if (isDay()) return null;
-        return null;
-    }
-
-    return (
-        <div className="orders__main-container">
-            {errorDates ?
-                <div>error occurred, please try again later</div>
-                : isLoadingDates ? <Loader/> :
-                    <>
-                        <OrdersNewOrderContainer refresh={refresh}/>
-                        <OrdersPathHeader
-                            year={year}
-                            month={month}
-                            monthName={getMonthName(month)}
-                            day={day}
-                            dayName={getDayName(year, month, day)}
-                        />
-                        <div className="orders__main-container__content">
-                            <OrdersSideList
-                                data={getSideListElements()}/>
-                            {isDay() ?
-                                <OrdersMainOrdersList
-                                    refresh={refresh}
-                                    orders={orders}
-                                    isLoading={isLoadingOrders}
-                                    error={errorOrders}/>
-                                :
-                                <OrdersMainDatesList
-                                    data={getMainListElements()}
-                                />
-                            }
-                        </div>
-                    </>
-            }
-        </div>
-    )
+    // const {
+    //     data: dates,
+    //     error: errorDates,
+    //     isLoading: isLoadingDates,
+    //     refetch: refetchDates
+    // } = useGetDates();
+    //
+    // const {
+    //     data: orders,
+    //     isLoading: isLoadingOrders,
+    //     error: errorOrders,
+    //     refetch: refetchOrders
+    // } = useGetOrders(year, month, day);
+    //
+    // const refresh = useCallback(() => {
+    //     refetchDates().then();
+    //     refetchOrders().then();
+    // }, [refetchDates, refetchOrders]);
+    //
+    // const isDay = () => day;
+    // const isMonth = () => month && !day;
+    // const isYear = () => year && !month;
+    //
+    // useEffect(() => {
+    //     if (dates && dates.status && dates.message) {
+    //         setYears(getYearsListFromServerResponse(dates, year));
+    //         setMonths(getMonthsListFromServerResponse(dates, year, month))
+    //         setDays(getDaysListFromServerResponse(dates, year, month, day));
+    //     }
+    // }, [dates, year, month, day]);
+    //
+    // function getSideListElements() {
+    //     if (!dates) return;
+    //     if (isYear()) return years;
+    //     if (isMonth()) return months;
+    //     if (isDay()) return days;
+    //     return years;
+    // }
+    //
+    // function getMainListElements() {
+    //     if (isYear()) return months;
+    //     if (isMonth()) return days;
+    //     if (isDay()) return null;
+    //     return null;
+    // }
+    //
+    // return (
+    //     <div className="orders__main-container">
+    //         {errorDates ?
+    //             <div>error occurred, please try again later</div>
+    //             : isLoadingDates ? <Loader/> :
+    //                 <>
+    //                     <OrdersNewOrderContainer refresh={refresh}/>
+    //                     <OrdersPathHeader
+    //                         year={year}
+    //                         month={month}
+    //                         monthName={getMonthName(month)}
+    //                         day={day}
+    //                         dayName={getDayName(year, month, day)}
+    //                     />
+    //                     <div className="orders__main-container__content">
+    //                         <OrdersSideList
+    //                             data={getSideListElements()}/>
+    //                         {isDay() ?
+    //                             <OrdersMainOrdersList
+    //                                 refresh={refresh}
+    //                                 orders={orders}
+    //                                 isLoading={isLoadingOrders}
+    //                                 error={errorOrders}/>
+    //                             :
+    //                             <OrdersMainDatesList
+    //                                 data={getMainListElements()}
+    //                             />
+    //                         }
+    //                     </div>
+    //                 </>
+    //         }
+    //     </div>
+    // )
+    return <div>Hello</div>
 }
